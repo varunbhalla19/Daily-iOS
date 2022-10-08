9f6ae5024c83269c65a1ea93475585379a38c13c
varunbhalla19 <varunbhalla19@gmail.com>
2022-10-08 12:44:28 +0530

    moved cell registration in a separate extension, added button and date-text to list item

--- Daily/TaskListViewController.swift
+++ Daily/TaskListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TaskListViewController.swift
 //  Daily
 //
 //  Created by varunbhalla19 on 06/10/22.
@@ -11,21 +11,13 @@ class TaskListViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
-    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
-    typealias SnapShot = NSDiffableDataSourceSnapshot<Int, String>
-    
     var dataSource: DataSource!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.collectionViewLayout = listLayout()
         
-        let cellRegistration = UICollectionView.CellRegistration { (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
-            var config = cell.defaultContentConfiguration()
-            let task = Task.testData[indexPath.item]
-            config.text = task.title
-            cell.contentConfiguration = config
-        }
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
         
         dataSource = DataSource.init(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
             collectionView.dequeueConfiguredReusableCell(
@@ -49,3 +41,46 @@ class TaskListViewController: UIViewController {
     
 }
 
+
+extension TaskListViewController {
+    
+    
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias SnapShot = NSDiffableDataSourceSnapshot<Int, String>
+
+    func cellRegistrationHandler(cell: UICollectionViewListCell,
+                                 indexPath: IndexPath, itemIdentifier: String) {
+        var config = cell.defaultContentConfiguration()
+        let task = Task.testData[indexPath.item]
+        config.text = task.title
+        config.secondaryText = task.dueDate.dateTimeString
+        config.secondaryTextProperties.font = .preferredFont(forTextStyle: .caption1)
+        cell.contentConfiguration = config
+        
+        var doneButtonConfig = doneButtonConfig(for: task)
+        doneButtonConfig.tintColor = .systemIndigo
+        cell.accessories = [
+            .customView(configuration: doneButtonConfig),
+            .disclosureIndicator(displayed: .always)
+        ]
+        
+        var backgroundConfig = UIBackgroundConfiguration.listGroupedCell()
+        backgroundConfig.backgroundColor = .systemGray6
+        cell.backgroundConfiguration = backgroundConfig
+    }
+    
+    func doneButtonConfig(for task: Task) -> UICellAccessory.CustomViewConfiguration {
+        
+        let circle = task.isComplete ? "circle.fill": "circle"
+        let imageConfig = UIImage.SymbolConfiguration(textStyle: .title1)
+        let image = UIImage.init(systemName: circle, withConfiguration: imageConfig)
+        let btn = UIButton.init()
+        btn.setImage(image, for: .normal)
+        
+        return .init(
+            customView: btn,
+            placement: .leading(displayed: .always)
+        )
+    }
+    
+}
