c99538f76a1c30404ad587eb108caf6a32810f84
varunbhalla19 <varunbhalla19@gmail.com>
2022-10-08 11:35:48 +0530

    added init snapshot, datasource and listconfig

--- Daily/TaskListViewController.swift
+++ Daily/TaskListViewController.swift
@@ -11,10 +11,33 @@ class TaskListViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias SnapShot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    var dataSource: DataSource!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.collectionViewLayout = listLayout()
         
+        let cellRegistration = UICollectionView.CellRegistration { (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            var config = cell.defaultContentConfiguration()
+            let task = Task.testData[indexPath.item]
+            config.text = task.title
+            cell.contentConfiguration = config
+        }
+        
+        dataSource = DataSource.init(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            collectionView.dequeueConfiguredReusableCell(
+                using: cellRegistration, for: indexPath, item: itemIdentifier)
+        })
+        
+        var snapShot = SnapShot.init()
+        snapShot.appendSections([0])
+        snapShot.appendItems(Task.testData.map({ $0.title }))
+        dataSource.apply(snapShot, animatingDifferences: true)
+        
+        collectionView.dataSource = dataSource
     }
 
     private func listLayout() -> UICollectionViewLayout {
